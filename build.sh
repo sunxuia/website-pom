43d57ffada9499300d5fc964982bbd951149c239
sunxuia <ntsunxu@163.com>
2019-09-06 14:00:28 +0800

    add all plugins to this pom

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
-    mvn clean deploy -Dgithub.global.oauth2Token=$github_token
+    mvn clean deploy -Dskip.site=false -Dgithub.global.oauth2Token=$github_token
 fi
