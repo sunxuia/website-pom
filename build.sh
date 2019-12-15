5952433650a6365be38c9336645968cda43569b8
sunxu <ntsunxu@163.com>
2019-12-15 16:46:25 +0800

    switch maven repo to github packages

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
-    mvn clean deploy -Dskip.site=false -Dgithub.global.oauth2Token=$github_token
+    cp .travis.settings.xml $HOME/.m2/settings.xml
+    mvn clean deploy -Dskip.site=false
 fi
